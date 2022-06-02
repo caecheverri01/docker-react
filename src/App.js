630f8366e91d06da1722878606bad8629a502554
Carlos Echeverri <caecheverri01@gmail.com>
2022-06-02 18:43:22 -0500

    modify app and some travis configurations

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there!!!
+          I was changed to use branching
         </p>
         <a
           className="App-link"
